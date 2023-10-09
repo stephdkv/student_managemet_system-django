209d8896508f3f5b667b035c160bf1e7fa3c2518
Stepan <freenlol@gmail.com>
2023-10-10 00:26:05 +0600

    'base-template'

--- sms/sms/urls.py
+++ sms/sms/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .import views, Hod_Views, Student_Views, Stuff_Views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('base/', views.BASE, name='base')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
